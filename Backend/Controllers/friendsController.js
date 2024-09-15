1d47850fa6e1302c37e4985209eb64e085bd6e00
vk-develops <vimalkumarv291@gmail.com>
2024-09-15 22:15:13 +0530

    added the show friends list api

--- Backend/Controllers/friendsController.js
+++ Backend/Controllers/friendsController.js
@@ -1,6 +1,35 @@
 import asyncHandler from "express-async-handler";
 import User from "../Models/userModel.js";
 
+// @desc    Show All Friends
+// @route   POST /api/v1/users/friends/show-friends
+// @access  Private
+const showAllFriends = asyncHandler(async (req, res) => {
+    try {
+        const userId = req.user._id;
+
+        const user = await User.findById(userId);
+
+        if (user) {
+            const friends = await User.populate("friends", "name email");
+
+            res.status(200).json({
+                success: true,
+                message: "Friends List Generated",
+                data: { count: friends.length, friends: friends },
+            });
+        } else {
+            res.status(400).json({
+                success: false,
+                message: "User does not exists!",
+            });
+        }
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).json({ success: false, err: err.message });
+    }
+});
+
 // @desc    Send friend request
 // @route   POST /api/v1/users/friends/send-request
 // @access  Private
@@ -17,7 +46,7 @@ const sendRequest = asyncHandler(async (req, res) => {
             await targetUser.save();
             res.status(200).json({
                 success: true,
-                message: "Friend request sent.",
+                message: "Friend request sent!",
             });
         } else {
             res.status(404).json({
@@ -31,4 +60,43 @@ const sendRequest = asyncHandler(async (req, res) => {
     }
 });
 
-export { sendRequest };
+// @desc    Accept friend request
+// @route   POST /api/v1/users/friends/accept-request
+// @access  Private
+const acceptFriend = asyncHandler(async (req, res) => {
+    try {
+        const user = req.user._id;
+
+        const { targetUser } = req.params;
+
+        const reqSentUser = await User.findById(targetUser);
+        const reqAcceptUser = await User.findById(user);
+
+        if (reqAcceptUser && reqSentUser) {
+            reqAcceptUser.friends.push(reqSentUser);
+            reqSentUser.friends.push(reqAcceptUser);
+
+            //Removing request from the reqSentUser's document
+            reqSentUser.friendRequests = reqSentUser.friendRequests.filter(
+                (requestId) => requestId.toString() != reqAcceptUser
+            );
+
+            await reqAcceptUser.save();
+            await reqSentUser.save();
+
+            res.status(200).json({
+                success: true,
+                message: "Friend Request Accepted!",
+            });
+        } else {
+            res.status(400).json({
+                success: false,
+                message: "User does not exists",
+            });
+        }
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).json({ success: false, err: err.message });
+    }
+});
+export { showAllFriends, sendRequest, acceptFriend };
--- Backend/Routes/friendsRoute.js
+++ Backend/Routes/friendsRoute.js
@@ -1,11 +1,18 @@
 import express from "express";
-import { sendRequest } from "../Controllers/friendsController";
+import {
+    acceptFriend,
+    sendRequest,
+    showAllFriends,
+} from "../Controllers/friendsController.js";
+import { protect } from "../Middlewares/authMiddleware.js";
 
 //Router init
 const router = express.Router();
 
 //HTTP Methods
-router.post("/send-request", protect, sendRequest);
+router.get("/show-friends", protect, showAllFriends);
+router.post("/send-request/:id", protect, sendRequest);
+router.post("/accept-request/:id", protect, acceptFriend);
 
 //Export
 export default router;
